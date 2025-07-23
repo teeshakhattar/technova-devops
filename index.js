43b1f077ee6d96d3c08066a4c32b041887c77407
Teesha Khattargit config --global user.email teeshakhattar13@gmail.com <khatt@INV106182.INV106182>
2025-07-24 03:22:51 +0530

    Initial commit with Teesha's changes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// Edited by Teesha Khattar - July 2025
 const express = require("express");
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello shreya using Docker!!!</h1>");
+    res.send("<h1>Hello teesha using Docker!!!</h1>");
 });
 
 app.listen(PORT, () => {
